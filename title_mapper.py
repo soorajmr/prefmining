84f0538e24a7ce90a78043d457a790352e82fd8a
Sooraj Raveendran <soorajmr@gmail.com>
2015-12-18 21:51:44 +0530

    title mapper changed to use pickle

--- title_mapper.py
+++ title_mapper.py
@@ -1,3 +1,8 @@
+
+# coding: utf-8
+
+# In[14]:
+
 # Match movie titles in Movielens and Wikipedia data
 # Code adapted from https://github.com/soorajmr/moviemeta/blob/master/notebooks/link_movies.ipynb
 #
@@ -5,6 +10,7 @@
 import re
 import pandas as pd
 from fuzzywuzzy import process
+import pickle
 
 def extract_year(title):
     match = re.search(r'.*\(([0-9]{4})\)', title)
@@ -20,11 +26,19 @@ ml_titlemap = dict(zip(movies.movieId, movies.title))
 ml_yearmap = dict(zip(movies.movieId, movies.year))
 #movies.year.plot(kind='hist', bins=range(1900, 2020, 5)).set_title("Movies per year")
 
-pref = pd.read_csv("data/output/pref_vectors.csv")
-content = pd.read_csv("data/output/content_vectors.csv")
-content['year'] = content.title.apply(lambda x: x.decode('utf-8')).apply(extract_year)
-pref['year'] = pref.apply(lambda x: ml_yearmap[x.movieId], axis=1)
-pref['title'] = pref.apply(lambda x: ml_titlemap[x.movieId].decode('utf-8'), axis=1)
+with open('data/output/content_titles.pickle', 'rb') as f:
+    ctitles = pickle.load(f)
+        
+with open('data/output/pref_movieids.pickle', 'rb') as f:
+    pmovieids = pickle.load(f)
+
+pref = pd.DataFrame(dict(title = [ml_titlemap[mid].decode('utf-8') for mid in pmovieids], 
+                         year = [ml_yearmap[mid] for mid in pmovieids]))
+
+content = pd.DataFrame(dict(title = ctitles, year = [extract_year(t.decode('utf-8')) for t in ctitles]))
+
+
+# In[15]:
 
 def fuzzy_find_matching_index(string, string_list):
     fuzzy_match, similarity_pct = process.extractOne(string, string_list)
@@ -32,7 +46,7 @@ def fuzzy_find_matching_index(string, string_list):
     # Boost the similarity of long strings
     if(similarity_pct >= 90):
         similarity_pct += len(string) / 4
-        
+
     if(similarity_pct >= 95): # Take only the ones with high similarity
         #print(string, "\t\t", fuzzy_match, "\t\t", similarity_pct)
         return string_list.index(fuzzy_match)
@@ -45,17 +59,18 @@ def create_title_map(wiki, mv):
     titlemap = mv.title.apply(lambda s: fuzzy_find_matching_index(s, list(wiki.title)))
     titlemap = titlemap.dropna()
     titlemap = titlemap.apply(lambda i: wiki.index[i])
-    return titlemap.astype(int).to_dict()    
+    return titlemap.astype(int).to_dict()
 
 mv2wiki = {}
 for y in range(2000, 2015):
     mv2wiki.update(create_title_map(content[content.year == y], pref[pref.year == y]))
     print("processed year %d, total %d" % (y, len(mv2wiki)))
 
-
-import pickle
-
 with open('data/output/mv2wiki.pickle', 'wb') as f:
     pickle.dump(mv2wiki, f)
+
+
+# In[ ]:
+
 
 
